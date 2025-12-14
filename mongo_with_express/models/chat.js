2f057865f34eea6a5c4cad695881fc590844b280
aman godara <amangodara005@gmail.com>
2025-12-14 15:08:52 +0530

    models folder

---
+++ mongo_with_express/models/chat.js
@@ -0,0 +1,7 @@
+//professional we will store all the collection in model folder
+//we will require everything in main index.js file only
+const mongoose = require("mongoose");
+const schea = mongoose.Schema;
+const chatSchema = new Schema ({
+    
+})
