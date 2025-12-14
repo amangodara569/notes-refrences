1047b7e30bfafe9d830aff000ebdfc487879bff4
aman godara <amangodara005@gmail.com>
2025-12-14 15:31:12 +0530

    created schema

--- mongo_with_express/models/chat.js
+++ mongo_with_express/models/chat.js
@@ -4,5 +4,28 @@ const mongoose = require("mongoose");
 const schea = mongoose.Schema;
 const chatSchema = new Schema ({
     //chats will have (id, from , to , message, created_at(date format others in string))
-    from : String,
-})
+
+    //first create the default schema , the go with adding multiple constraints as needed
+    //like we always need from , to , created_at = required:true
+    from : {
+        type : String,
+        required : true,
+    },
+    to : {
+        type : String,
+        required : true,
+    },
+    message : {
+        type : String,
+        maxLength : 50,
+    },
+    created_at : {
+        type : date,
+        required : true,
+    }
+});
+
+
+const Chat = mongoose.model("Chat",chatSchema);
+
+module.exports = Chat;
