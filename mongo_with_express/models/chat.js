da4ac2c822574b879425780e55c3cbdb1fbd103c
aman godara <amangodara005@gmail.com>
2025-12-14 15:14:21 +0530

    models folder

--- mongo_with_express/models/chat.js
+++ mongo_with_express/models/chat.js
@@ -3,5 +3,6 @@
 const mongoose = require("mongoose");
 const schea = mongoose.Schema;
 const chatSchema = new Schema ({
-    
+    //chats will have (id, from , to , message, created_at(date format others in string))
+
 })
